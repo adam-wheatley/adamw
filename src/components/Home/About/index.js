c96a809e6431fabffd3ab70a39d9d8f6834e415d
adamwheatley-twig <awheatley@twig-world.com>
2019-02-23 22:41:16 +0000

    Make more responsive

--- src/components/Home/About/index.js
+++ src/components/Home/About/index.js
@@ -8,13 +8,13 @@ const About = () => {
         <div className="about">
             <div className="container">
             <div className="row">
-                <div className="col about__header">
+                <div className="col-sm-12 col-md-6 about__header">
                     <ScrollAnimation offset={-150} animateIn="fadeInLeft" duration="2" delay="1000" animateOnce>
                         <h1>Freelance Web Developer</h1>
                         <div className="about__header-pill"></div>
                     </ScrollAnimation>
                 </div>
-                <div className="col about__description">
+                <div className="col-sm-12 col-md-6 about__description">
                 <ScrollAnimation offset={-150} animateIn="fadeInRight" duration="2" delay="1000" animateOnce>
                     <p>
                         I'm Adam, a full-time web developer based in Scotland <img alt="scottish-flag" src={flag} height="25" className="mr-2" />
--- src/components/Home/CaseStudies/index.js
+++ src/components/Home/CaseStudies/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import barber from "./barber.png"
 import Designer from './designer.svg'
 import Dev from './dev.svg'
 import Deploy from './deploy.svg'
@@ -8,31 +7,27 @@ import "./CaseStudies.scss"
 const CaseStudies = () => {
   return (
     <div className="case-studies">
-      <div className="case-studies__header">
-        <h1>Services</h1>
-        <div className="about__header-pill mx-auto" />
-      </div>
       <div className="container">
-        <div className="row case-studies__info">
-          <div className="col">
+        <div className="row flex-column-reverse flex-md-row case-studies__info">
+          <div className="col-sm-12 col-md-6">
             <h1>Web Design</h1>
-            <div className="about__header-pill" />
+            <div className="case-studies__header-pill" />
             <p className="case-studies__info-description">
               If you are looking for a Hiqh Quality, Modern look and feeling website then I am the man for you. Keeping up with the newest design trends I make sure I provide web solutions that users enjoy.
               Looking for a Modern, Sleek looking Website? Keeping up with up to date trends I provide high quality user experiences.
             </p>
           </div>
-          <div className="col text-center">
+          <div className="col-sm-12 col-md-6 text-center">
             <img src={Designer} height="250" className="case-studies__info-img" />
           </div>
         </div>
         <div className="row case-studies__info">
-          <div className="col text-center">
+          <div className="col-sm-12 col-md-6 text-center">
             <img src={Dev} height="225" className="case-studies__info-img" />
           </div>
-          <div className="col">
+          <div className="col-sm-12 col-md-6">
             <h1>Web Development</h1>
-            <div className="about__header-pill" />
+            <div className="case-studies__header-pill" />
             <p className="case-studies__info-description">
               Hey, I’m <b>Adam</b>. I am a freelance <b>web developer</b> that
               aim's to provide hiqh quality, unqiue & custom web solutions to
@@ -41,15 +36,15 @@ const CaseStudies = () => {
           </div>
         </div>
 
-        <div className="row case-studies__info">
-          <div className="col">
+        <div className="row flex-column-reverse flex-md-row case-studies__info">
+          <div className="col-sm-12 col-md-6">
             <h1>Deployment</h1>
-            <div className="about__header-pill" />
+            <div className="case-studies__header-pill" />
             <p className="case-studies__info-description">
               Using industry standard practices I create web solutions that are highly performant and deployed to fast and secure servers.
             </p>
           </div>
-          <div className="col text-center">
+          <div className="col-sm-12 col-md-6 text-center">
             <img src={Deploy} height="250" className="case-studies__info-img" />
           </div>
         </div>
