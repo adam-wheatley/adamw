795d0775341a172f65b3d3f6d232d815aff17df7
adamwheatley-twig <awheatley@twig-world.com>
2019-02-25 16:39:43 +0000

    Remove Contact Form

--- src/components/Contact/Header/index.js
+++ src/components/Contact/Header/index.js
@@ -6,52 +6,10 @@ const ContactHeader = () => {
     <div className="contact-header">
       <div className="contact-header__header">
         <h1 className="contact-header__header-title">Get in touch</h1>
-        <p className="contact-header__header-description">
-          If you are interested in hiring me, <br />
-          or just have some questions please get in touch.
-        </p>
-          <form
-            className="contact-header__form"
-            name="contact"
-            method="POST"
-            data-netlify="true"
-          >
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control contact-header__form-control"
-                name="name"
-                placeholder="Full Name"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="email"
-                className="form-control contact-header__form-control"
-                name="email"
-                placeholder="Email Address"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control contact-header__form-control"
-                name="telephpne"
-                placeholder="Telephone"
-              />
-            </div>
-            <div className="form-group">
-              <textarea
-                type="text"
-                className="form-control contact-header__form-control"
-                name="message"
-                placeholder="Leave a message"
-              />
-            </div>
-            <button type="submit" className="contact-header__form-btn">
-              Submit
-            </button>
-          </form>
+          <p className="contact-header__header-description">
+            If you are interested in hiring me, <br />
+            or just have some questions please get in touch.
+          </p>
         </div>
       </div>
   )
