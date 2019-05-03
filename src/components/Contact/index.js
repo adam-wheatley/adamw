ffa5d13b84564556cbf380df29294734561d3c21
awheatley@twig-world.com <awheatley@twig-world.com>
2019-05-03 15:15:44 +0100

    Update Contact Page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,52 +1,11 @@
 import React from "react"
+import Header from './Header'
 import "./Contact.scss"
 
 const Contact = () => {
   return (
     <div className="contact">
-      <div className="contact__header">
-        <h1 className="contact__header-title">Get in touch</h1>
-        <p className="contact__header-description">
-          If you are interested in hiring me, <br />
-          or just have some questions please get in touch.
-        </p>
-      </div>
-      <div className="container">
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          className="contact__form"
-        >
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control contact__form-control"
-              name="name"
-              placeholder="Full Name"
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control contact__form-control"
-              name="telephpne"
-              placeholder="Telephone"
-            />
-          </div>
-          <div className="form-group">
-            <textarea
-              type="text"
-              className="form-control contact__form-control"
-              name="message"
-              placeholder="Leave a message"
-            />
-          </div>
-          <button type="submit" className="contact__form-btn">
-            Submit
-          </button>
-        </form>
-      </div>
+      <Header />
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,12 +3,10 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Contact from '../components/Contact'
-import Nav from '../components/Nav'
 
 const ContactPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`web`, `development`, `software`, `design`, `freelance`, `scotland`]} />
-    <Nav />
     <Contact />
   </Layout>
 )
